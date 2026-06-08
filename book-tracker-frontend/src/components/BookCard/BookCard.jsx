e35e7d0d7b5cfa3b372351a599c53b87ed81e4db
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-08 17:36:15 -0400

    refactor: Prepara tarjeta para rellenarse con datos de la api.

--- book-tracker-frontend/src/components/BookCard/BookCard.jsx
+++ book-tracker-frontend/src/components/BookCard/BookCard.jsx
@@ -1,35 +1,32 @@
 import exampleImg from "../../images/example.webp";
 
-export default function BookCard({ bookCard, onCardClose }) {
-  function handleCloseClick() {
-    onCardClose(bookCard);
-  }
+export default function BookCard({ book, onCardClose }) {
+  const info = book.volumeInfo;
+
+  const title = info.title || "Título desconocido";
+  const author = info.authors ? info.authors[0] : "Autor desconocido";
+  const synopsis = info.description || "Sin sinopsis disponible.";
+  const pages = info.pageCount || "N/A";
+  const genre = info.categories ? info.categories[0] : "General";
+  const coverImage = info.imageLinks?.thumbnail || exampleImg;
+
   return (
     <li className="book-card">
-      <img className="book-card__image" src={exampleImg} />
+      <img className="book-card__cover" src={coverImage} />
       <button
         aria-label="Cerrar modal"
         className="book-card__close-button"
         type="button"
-        onClick={handleCloseClick}
+        onClick={() => onCardClose(book)}
       ></button>
       <ul className="book-card__description">
         <span className="book-card__chip book-card__chip--state">Leyendo</span>
-        <li className="book-card__title">
-          El señor de los anillos: La comunidad del anillo
-        </li>
-        <li className="book-card__author">J.R.R. Tolkien</li>
-        <li className="book-card__synopsis">
-          El mago Gandalf descubre que el anillo heredado por Frodo de su tío
-          Bilbo es el Anillo Único creado por Sauron. Para evitar que el mal se
-          apodere del mundo, se forma una comunidad de nueve miembros
-          —incluyendo humanos, elfos, enanos y hobbits— que parte hacia Mordor.
-          Sin embargo, la corrupción del anillo y los ataques de los orcos
-          terminan separando al grupo.
-        </li>
+        <li className="book-card__title">{title}</li>
+        <li className="book-card__author">{author}</li>
+        <li className="book-card__synopsis">{synopsis}</li>
         <li className="book-card__chip">
-          <li className="book-card__chip book-card__chip--pages">576</li>
-          <li className="book-card__chip book-card__chip--genre">Fantasía</li>
+          <li className="book-card__chip book-card__chip--pages">{pages}</li>
+          <li className="book-card__chip book-card__chip--genre">{genre}</li>
         </li>
       </ul>
     </li>
