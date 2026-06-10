7e7ca262f8904303f593755fc6ec42b9ba5de58d
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-10 10:34:57 -0400

    Refactor: cambia el nombre del side bar a Navigation.

---
+++ book-tracker-frontend/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.svg";
+
+export default function Navigation({
+  loggedIn,
+  userEmail,
+  userName = "Usuario",
+  onEditAvatarClick,
+  userAvatar,
+}) {
+  const defaultAvatar = `https://api.dicebear.com/9.x/pixel-art/svg?seed=${userName}`;
+  const finalAvatarUrl = userAvatar ? userAvatar : defaultAvatar;
+
+  return (
+    <aside className="navigation">
+      <Link to="/" className="navigation__logo-link">
+        <img src={logo} alt="Logo" className="navigation__logo" />
+      </Link>
+      <div className="navigation__profile">
+        <img
+          src={finalAvatarUrl}
+          alt={`Avatar de ${userName}`}
+          className="navigation__avatar"
+          onClick={onEditAvatarClick}
+        />
+        <h2 className="navigation__greeting">Hola, {userName}</h2>
+      </div>
+
+      {loggedIn && (
+        <>
+          <nav className="navigation__nav">
+            <Link to="/" className="navigation__link">
+              Mi Biblioteca
+            </Link>
+            <Link to="/search" className="navigation__link">
+              Buscar Libros
+            </Link>
+            <Link to="/profile" className="navigation__link">
+              Mi perfil
+            </Link>
+          </nav>
+
+          <div className="navigation__footer">
+            <span className="navigation__email">{userEmail}</span>
+          </div>
+        </>
+      )}
+    </aside>
+  );
+}
