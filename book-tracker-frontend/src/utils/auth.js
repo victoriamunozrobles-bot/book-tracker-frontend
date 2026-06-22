60173e04e77c7a0b5b81486cacce5498ca7d03e0
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-22 15:38:40 -0400

    Feat: agrega mockAPI para la autenticación de usuarios.

--- book-tracker-frontend/src/utils/auth.js
+++ book-tracker-frontend/src/utils/auth.js
@@ -1,33 +1,50 @@
-//export const BASE_URL = "http://localhost:5173";
+export const BASE_URL = "https://6a398c2864a2d82692241d25.mockapi.io/users";
 
-export const register = (name, email, password) => {
-  console.log("💻 Servidor Falso: Registrando a", name);
+export const register = async (name, email, password) => {
+  const resGet = await fetch(BASE_URL);
+  const users = await resGet.json();
 
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ data: { _id: "123", email: email, name: name } });
-    }, 1000);
+  const userExists = users.some((user) => user.email === email);
+
+  if (userExists) {
+    alert("Este correo ya está registrado. Por favor, inicia sesión.");
+    return Promise.reject("El usuario ya existe");
+  }
+
+  const resPost = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, password }),
   });
+
+  if (resPost.ok) {
+    return resPost.json();
+  } else {
+    return Promise.reject(`Error: ${resPost.status}`);
+  }
 };
 
-export const authorize = (email, password) => {
-  console.log("💻 Servidor Falso: Iniciando sesión de", email);
+export const authorize = async (email, password) => {
+  const res = await fetch(BASE_URL);
+  const users = await res.json();
 
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ token: "mi-token-super-secreto-12345" });
-    }, 1000);
-  });
+  const validUser = users.find(
+    (user) => user.email === email && user.password === password,
+  );
+
+  if (validUser) {
+    return { token: "token-simulado-mockapi-" + validUser.id };
+  } else {
+    alert("Correo o contraseña incorrectos.");
+    return Promise.reject("Credenciales inválidas");
+  }
 };
 
-export const checkToken = (token) => {
-  console.log("💻 Servidor Falso: Validando el token guardado...");
-
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        data: { email: "usuario@ejemplo.com", name: "Lector Estrella" },
-      });
-    }, 500);
-  });
+export const checkToken = async (token) => {
+  if (token && token.startsWith("token-simulado-mockapi-")) {
+    return { data: { email: "usuario@mockapi.com" } };
+  }
+  return Promise.reject("Token inválido");
 };
