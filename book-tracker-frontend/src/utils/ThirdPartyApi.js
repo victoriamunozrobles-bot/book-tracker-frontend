9ec775e482b0f9df589275029a11de36ed8b3ac3
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-22 15:58:34 -0400

    Feat: agrega validación de datos del usuario en el login y registro.

--- book-tracker-frontend/src/utils/ThirdPartyApi.js
+++ book-tracker-frontend/src/utils/ThirdPartyApi.js
@@ -2,21 +2,26 @@ const GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes";
 
 const API_KEY = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
 
-export const searchBooks = (query) => {
-  const formattedQuery = query.split(" ").join("+");
+export const searchBooks = async (query) => {
+  try {
+    const response = await fetch(
+      `${GOOGLE_BOOKS_API_URL}?q=${query}&key=${API_KEY}`,
+    );
 
-  return fetch(
-    `${GOOGLE_BOOKS_API_URL}?q=${formattedQuery}&maxResults=12&key=${API_KEY}`,
-  )
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(
-        `Error al contactar con Google Books: ${res.status}`,
-      );
-    })
-    .then((data) => {
-      return data.items || [];
-    });
+    if (!response.ok) {
+      throw new Error(`Error en la petición: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (!data.items || data.items.length === 0) {
+      console.warn("La búsqueda no devolvió resultados.");
+      return [];
+    }
+
+    return data.items;
+  } catch (error) {
+    console.error("Error al comunicarse con la API de Google Books:", error);
+    throw error;
+  }
 };
