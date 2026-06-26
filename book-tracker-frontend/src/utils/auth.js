e28494f4b489474a9ee4dd602d9a647e7bf8c7bb
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-26 10:41:56 -0400

    fix: actualiza BASE_URL con el endpoint correcto y refactoriza el manejo de la respuesta en la función de registro.

--- book-tracker-frontend/src/utils/auth.js
+++ book-tracker-frontend/src/utils/auth.js
@@ -1,51 +1,44 @@
-export const BASE_URL = "https://6a398c2864a2d82692241d25.mockapi.io/users";
+export const BASE_URL = "https://book-tracker.mooo.com";
+
+const checkResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error: ${res.status}`);
+};
 
 export const register = async (name, email, password) => {
-  const resGet = await fetch(BASE_URL);
-  const users = await resGet.json();
-
-  const userExists = users.some((user) => user.email === email);
-
-  if (userExists) {
-    alert("Este correo ya está registrado. Por favor, inicia sesión.");
-    return Promise.reject("El usuario ya existe");
-  }
-
-  const resPost = await fetch(BASE_URL, {
+  const res = await fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
+      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ name, email, password }),
   });
-
-  if (resPost.ok) {
-    return resPost.json();
-  } else {
-    return Promise.reject(`Error: ${resPost.status}`);
-  }
+  return checkResponse(res);
 };
 
 export const authorize = async (email, password) => {
-  const res = await fetch(BASE_URL);
-  const users = await res.json();
-
-  const validUser = users.find(
-    (user) => user.email === email && user.password === password,
-  );
-
-  if (validUser) {
-    return { token: `token-simulado-${validUser.email}` };
-  } else {
-    alert("Correo o contraseña incorrectos.");
-    return Promise.reject("Credenciales inválidas");
-  }
+  const res = await fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  return checkResponse(res);
 };
 
 export const checkToken = async (token) => {
-  if (token && token.startsWith("token-simulado-")) {
-    const userEmail = token.replace("token-simulado-", "");
-    return { data: { email: userEmail } };
-  }
-  return Promise.reject("Token inválido");
+  const res = await fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return checkResponse(res);
 };
