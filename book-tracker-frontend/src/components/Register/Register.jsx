6bb09fde20314fd449464bc8f7f6eb08e0dd36bf
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-06-30 02:17:25 -0400

    Feat: add responsive design for App, Header, Login, and Register components

--- book-tracker-frontend/src/components/Register/Register.jsx
+++ book-tracker-frontend/src/components/Register/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useFormAndValidation } from "../../utils/useFormAndValidation";
-import "./Register.css";
+import "./register.css";
 
 export default function Register({
   isOpen,
