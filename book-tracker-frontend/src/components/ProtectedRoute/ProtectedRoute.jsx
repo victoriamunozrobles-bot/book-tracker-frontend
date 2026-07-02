e1b6bb20f2971d2d862c0c5ce0730511d1af1871
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-07-02 11:25:49 -0400

    Fix: corrige redirección del usuario cuando no está logeado, el popup de login se abre automáticamente en "/".

--- book-tracker-frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ book-tracker-frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,7 +1,11 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ loggedIn, children }) => {
-  return loggedIn ? children : <Navigate to="/signin" replace />;
+  return loggedIn ? (
+    children
+  ) : (
+    <Navigate to="/" replace state={{ requireLogin: true }} />
+  );
 };
 
 export default ProtectedRoute;
